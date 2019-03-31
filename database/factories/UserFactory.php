e4919be45dd67078590b2691e51dbc50cbdc2778
haisam <he3@njit.edu>
2019-03-30 23:12:12 -0400

    Setup sqlite databse, Auth

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,7 +17,8 @@ use Faker\Generator as Faker;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'First_name' => $faker->fistname,
+        'Last_name' => $faker->lastname,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
